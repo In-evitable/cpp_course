c1bc0f352f974e418d4ffabc01642437be0b0046
GamerGuy-52 <hamzaamr19970@gmail.com>
2021-09-10 11:07:36 +0400

    Modified 9.4

---
+++ Chapter 9/example_9_4/cmake-build-debug/CMakeFiles/example_9_4.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/example_9_4.dir/main.cpp.obj"
+  "example_9_4.exe"
+  "example_9_4.exe.manifest"
+  "example_9_4.lib"
+  "example_9_4.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/example_9_4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
